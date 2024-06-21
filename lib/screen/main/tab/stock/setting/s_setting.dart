8876ea3c4d3af3f3a68f21059110f766f1d4e117
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-06-21 17:20:25 +0900

    feat: create slider widget

--- lib/screen/main/tab/stock/setting/s_setting.dart
+++ lib/screen/main/tab/stock/setting/s_setting.dart
@@ -1,5 +1,8 @@
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/data/preference/prefs.dart';
+import 'package:fast_app_base/screen/main/tab/stock/setting/w_switch_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -17,6 +20,32 @@ class _SettingScreenState extends State<SettingScreen> {
         title: '설정'.text.white.bold.make(),
         backgroundColor: context.appColors.appBarBackGround,
         iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: ListView(
+        children: [
+          // switch
+          Obx(
+            () => SwitchMenu(
+              title: '푸시 설정',
+              isOn: Prefs.isPushOnRx.get(),
+              onChanged: (isOn) {
+                Prefs.isPushOnRx(isOn);
+              },
+            ),
+          ),
+          // slider
+          Obx(
+            () => Slider(
+              value: Prefs.sliderPosition.get(),
+              onChanged: (value) {
+                Prefs.sliderPosition.set(value);
+              },
+            ),
+          ),
+          // date time
+
+          // number
+        ],
       ),
     );
   }
---
+++ lib/screen/main/tab/stock/setting/w_switch_menu.dart
@@ -0,0 +1,30 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/stock/setting/w_os_switch.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SwitchMenu extends StatelessWidget {
+  final String title;
+  final bool isOn;
+  final ValueChanged<bool> onChanged;
+
+  const SwitchMenu({
+    super.key,
+    required this.title,
+    required this.isOn,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        title.text.white.bold.make(),
+        const Expanded(
+          child: SizedBox(),
+        ),
+        OsSwitch(value: isOn, onChanged: onChanged),
+      ],
+    ).p(20);
+  }
+}
