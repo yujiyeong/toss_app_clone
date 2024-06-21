9b18269e5dc2508dad0f17f268a5483634a506e6
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-06-21 17:40:25 +0900

    feat: create date setting widget

--- lib/common/data/preference/prefs.dart
+++ lib/common/data/preference/prefs.dart
@@ -3,9 +3,14 @@ import 'package:fast_app_base/common/data/preference/item/preference_item.dart';
 import 'package:fast_app_base/common/data/preference/item/rx_preference_item.dart';
 import 'package:fast_app_base/common/theme/custom_theme.dart';
 
+import 'item/rxn_preference_item.dart';
+
 class Prefs {
   static final appTheme = NullablePreferenceItem<CustomTheme>('appTheme');
   static final isPushOn = PreferenceItem<bool>('isPushOn', false);
   static final isPushOnRx = RxPreferenceItem<bool, RxBool>('isPushOn', false);
-  static final sliderPosition = RxPreferenceItem<double, RxDouble>('sliderPosition', 0.0);
+  static final sliderPosition =
+      RxPreferenceItem<double, RxDouble>('sliderPosition', 0.0);
+  static final birthday =
+      RxnPreferenceItem<DateTime, Rxn<DateTime>>('birthday');
 }
